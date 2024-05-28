9d6e8fc990a0ff298abea95bda153784034626f1
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-05-28 18:17:16 +0200

    a

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -39,7 +39,7 @@ const LoginForm = function () {
 
     const fetchLogin = async () => {
         try {
-            const response = await fetch(`${API_URL}/login`, {
+            const response = await fetch(`https:/aipluswebserver-vincenzocostantini-082c8784.koyeb.applogin`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
