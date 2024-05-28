b015baabf0a64d8eb2a15a231c357baec1e7354f
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-05-28 14:10:12 +0200

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  // Carica le variabili di ambiente
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    define: {
+      __APP_ENV__: env.APP_ENV,
+      'process.env': env
+    },
+    build: {
+      outDir: 'dist',
+    },
+  };
+});
